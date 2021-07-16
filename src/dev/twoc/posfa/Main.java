5f87d34eb7d27e059356a694da2fce7d77667cf6
Nil Pointer <TowardsTheNewWorld@outlook.com>
2021-07-16 21:53:14 +0800

    Initial commit

---
+++ src/dev/twoc/posfa/Main.java
@@ -0,0 +1,16 @@
+package dev.twoc.posfa;
+
+import com.google.gson.JsonParser;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/dev/twoc/posfa/PosAttributesGetter.java
@@ -0,0 +1,89 @@
+package dev.twoc.posfa;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import okhttp3.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+import static java.net.HttpURLConnection.HTTP_NOT_FOUND;
+import static java.net.HttpURLConnection.HTTP_OK;
+
+public class PosAttributesGetter {
+
+    public static final OkHttpClient client = new OkHttpClient.Builder()
+            .retryOnConnectionFailure(true)
+            .build();
+
+    public static void main(String[] args) {
+        JsonArray ids = getMarketGroups();
+        CountDownLatch count = new CountDownLatch(ids.size());//use this to wait until all async link finish.
+
+        for (JsonElement element : ids) {
+            getMarketGroup(element.getAsInt(), new Callback() {
+                @Override
+                public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                    getMarketGroup(element.getAsInt(), this);//if failed, retry
+                }
+
+                @Override
+                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                    count.countDown();
+                }
+            });
+        }
+
+        try {
+            count.await();//wait until count down to zero.
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static JsonArray getMarketGroups() {
+        return JsonParser.parseString(getResponseSync("https://esi.evetech.net/latest/markets/groups/?datasource=tranquility")).getAsJsonArray();
+    }
+
+    public static void getMarketGroup(int id, Callback callback) {
+        getResponseAsync("https://esi.evetech.net/latest/markets/groups/" + id + "/?datasource=tranquility&language=en", callback);
+    }
+
+    public static String getResponseSync(String url) {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Response response;
+        ResponseBody body;
+        try {
+            response = client.newCall(request).execute();
+            System.out.println("got response, code=" + response.code());
+            if (response.code() != HTTP_OK) {
+                return getResponseSync(url);//if failed, retry
+            }
+
+            body = response.body();
+            if (body != null) {
+                return body.string();
+            } else {
+                return "";
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return getResponseSync(url);//retry
+        }
+    }
+
+    public static void getResponseAsync(String url, Callback callback) {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        client.newCall(request).enqueue(callback);
+    }
+
+}
